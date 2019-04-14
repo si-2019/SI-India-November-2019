1e2b16e92da4705a27d441edbc7c7770175fa9f9
aldin-celik <acelik3@etf.unsa.ba>
2019-04-14 16:55:33 +0200

    Napravljena lista za predmete

--- MobilnaAplikacija/komponente/OdslusaniPredmeti.js
+++ MobilnaAplikacija/komponente/OdslusaniPredmeti.js
@@ -12,12 +12,29 @@ class OdslusaniPredmeti extends React.Component {
         }
     }
 
+    dohvatiPredmete()
+    {
+        /*
+        axios.get("urlapija/id="+this.props.idStudenta).then(
+            res => {
+                this.setState({predmeti : res.data});//Eventualno promjeniti ako bude drugi format podataka
+            }
+        )*/
+        var dummyLista = ["Inženjerska matematika 1", "Linearna algebra i geometrija", "Osnove elektrotehnike"];
+        this.setState({predmeti : dummyLista});
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+    }
+    componentDidMount()
+    {
+        this.dohvatiPredmete();
+    }
+
+
+    render() {
+        return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+        </View>
     );
   }
 }
