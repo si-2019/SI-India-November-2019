fe698fffad15e0cf1b8577d9e2f1ec1c5a57bc6b
Amina Kurtovic <akurtovic3@etf.unsa.ba>
2019-04-14 16:55:15 +0200

    Besplatne potvrde

--- MobilnaAplikacija/App.js
+++ MobilnaAplikacija/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import BesplatnePotvrde from './src/components/BesplatnePotvrde'
 export default class App extends React.Component {
   render() {
     return (
