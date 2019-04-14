86cc9a320f29af9cce616e67ac646a6c59619f5b
ekapetanovic1 <ekapetanov1@etf.unsa.ba>
2019-04-14 12:54:19 +0200

    Implementirana FlatList-a unutar komponente SubjectsList

--- MobilnaAplikacija/components/SubjectsList.js
+++ MobilnaAplikacija/components/SubjectsList.js
@@ -12,9 +12,52 @@ export class SubjectsList extends React.Component {
 
     render() {
         return (
-            <View>
-               <Text>SubjectsList Component!</Text>
+            <View style={styles.subjectsContainer}>
+                <FlatList 
+                    data={subjects}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={({ item }) => (
+                        <Text style={styles.item}>
+                            {item.title}
+                        </Text>
+                    )}
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    subjectsContainer: {
+        paddingTop: 100,
+        backgroundColor: 'skyblue'
+    },
+    item: {
+        padding: 5,
+        fontSize: 16,
+        height: 28
+    }
+});
+
+const subjects = [
+    {
+        id: 1,
+        title: "Organizacija softverskog projekta"
+    },
+    {
+        id: 2,
+        title: "Projektovanje informacionih sistema"
+    },
+    {
+        id: 3,
+        title: "Softver inženjering"
+    },
+    {
+        id: 4,
+        title: "Vještačka inteligencija"
+    },
+    {
+        id:5,
+        title: "Završni rad"
+    }
+]
