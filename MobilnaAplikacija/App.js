00453c0d3e1d20696ecd6ca609bb0badaf98bd40
ndardagan <nadja_dar@hotmail.com>
2019-04-14 19:42:02 +0200

    Ispravka vrijednosti za odabir

--- MobilnaAplikacija/App.js
+++ MobilnaAplikacija/App.js
@@ -9,24 +9,29 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      pickerSelection: "Potvrda o redovnom studiju"
+      pickerSelection: 0
     }
   }
 
   render() {
+    const lista = [{label: "Potvrda o redovnom studiju", value: "potvrda"},   
+                  {label: "Uvjerenje o položenim ispitima", value: "uvjerenje"}];
     return (
       <View>
         <Text style={{fontSize: RF(3.5), margin: 70, alignSelf: 'center'}}>Zahtjev za izdavanje ovjerenog uvjerenja</Text>
         <Text style={{fontSize: RF(2.5), alignSelf: 'center'}}>Izaberite tip potvrde: </Text>
         <Picker
-          selectedValue={this.state.pickerSelection}
+          selectedValue={lista[this.state.pickerSelection].value}
           mode='dropdown'
           style={{height: 80, width: 320, alignSelf: 'center'}}
           onValueChange={(itemValue, itemIndex) =>
-            this.setState({pickerSelection: itemValue})}
+            {this.setState({pickerSelection: itemIndex})}}
         >
-          <Picker.Item label="Potvrda o redovnom studiju" value="potvrda" />
-          <Picker.Item label="Uvjerenje o položenim ispitima" value="uvjerenje" />
+          {lista.map(element =>{
+            return (
+                <Picker.Item key={element.value} label={element.label} value={element.value}/>
+            );
+          })}
         </Picker>
       </View>
     );
