84ec0814628c8795e9f80f532125dcc7a1d24b09
aldin-celik <acelik3@etf.unsa.ba>
2019-04-14 16:57:31 +0200

    Revert "Napravljena lista za predmete"
    This reverts commit 1e2b16e92da4705a27d441edbc7c7770175fa9f9.

--- MobilnaAplikacija/komponente/OdslusaniPredmeti.js
+++ MobilnaAplikacija/komponente/OdslusaniPredmeti.js
@@ -12,29 +12,12 @@ class OdslusaniPredmeti extends React.Component {
         }
     }
 
-    dohvatiPredmete()
-    {
-        /*
-        axios.get("urlapija/id="+this.props.idStudenta).then(
-            res => {
-                this.setState({predmeti : res.data});//Eventualno promjeniti ako bude drugi format podataka
-            }
-        )*/
-        var dummyLista = ["Inženjerska matematika 1", "Linearna algebra i geometrija", "Osnove elektrotehnike"];
-        this.setState({predmeti : dummyLista});
 
-    }
-    componentDidMount()
-    {
-        this.dohvatiPredmete();
-    }
-
-
-    render() {
-        return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-        </View>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
     );
   }
 }
