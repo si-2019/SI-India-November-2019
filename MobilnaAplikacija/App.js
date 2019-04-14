624a73b91916f835458158807aa96398d2b81a3f
apanjeta2 <37374797+apanjeta2@users.noreply.github.com>
2019-04-14 21:33:13 +0200

    Update App.js

--- MobilnaAplikacija/App.js
+++ MobilnaAplikacija/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Profil from './ProfilStudenta/profilStudenta'
-
 export default class App extends React.Component {
   render() {
     return (
-      <Profil />
+        <View>
+            <Text>Hello </Text>
+        </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
