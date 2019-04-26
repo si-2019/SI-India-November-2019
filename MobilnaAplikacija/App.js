3a6620064e98589dbcac46162e88045b9fbe450e
Amina Kurtovic <akurtovic3@etf.unsa.ba>
2019-04-26 13:16:40 +0200

    prebaceno iz src-components u glavni app js

--- MobilnaAplikacija/App.js
+++ MobilnaAplikacija/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import BesplatnePotvrde from './src/components/BesplatnePotvrde'
+import {Button, Alert, StyleSheet, Text, View, Picker } from 'react-native';
+import RF from "react-native-responsive-fontsize"
+ 
+
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      besplatne_potvrde: 5
+    };
+  }
+
   render() {
+      
+    const jedna_bp = <Text>1 besplatna potvrda!</Text>
+    const pet_bp = <Text>5 besplatnih potvrda!</Text>
+    const izmedju_bp = ' besplatne potvrde!'
+    let kraj_rec;
+    let pocetak_recenice = <Text>Preostalo vam je </Text>
+    if(this.state.besplatne_potvrde == 5){
+      kraj_rec = pet_bp;
+    } else if(this.state.besplatne_potvrde == 1){
+      kraj_rec = jedna_bp;
+      pocetak_recenice=<Text>Preostala vam je </Text>
+    }
+    else if(this.state.besplatne_potvrde>=2 && this.state.besplatne_potvrde<=4){
+      kraj_rec= this.state.besplatne_potvrde.toString()+izmedju_bp;
+      pocetak_recenice=<Text>Preostale su vam </Text>
+    }
+    else{
+      pocetak_recenice=<Text></Text>
+      kraj_rec=<Text>Nemate više besplatnih potvrda!</Text>
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text style={{fontSize: RF(2.5), alignSelf: 'center', color:'red'}}>
+          <Text>{pocetak_recenice}</Text>
+          <Text>{kraj_rec}</Text>
+        </Text>
+          <Text></Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
