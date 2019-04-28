b3ce5edba5b3d71c03687f3307f2e91010aca6d2
ndardagan <nadja_dar@hotmail.com>
2019-04-28 15:04:10 +0200

    Dodan folder india

--- MobilnaAplikacija/App.js
+++ MobilnaAplikacija/App.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Picker } from 'react-native';
 import RF from "react-native-responsive-fontsize"
+import Potvrde from './India/Potvrde'
  
 
 export default class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pickerSelection: 0
-    }
-  }
-
+  
   render() {
-    const lista = [{label: "Potvrda o redovnom studiju", value: "potvrda"},   
-                  {label: "Uvjerenje o položenim ispitima", value: "uvjerenje"}];
+    
     return (
       <View>
-        <Text style={{fontSize: RF(3.5), margin: 70, alignSelf: 'center'}}>Zahtjev za izdavanje ovjerenog uvjerenja</Text>
-        <Text style={{fontSize: RF(2.5), alignSelf: 'center'}}>Izaberite tip potvrde: </Text>
-        <Picker
-          selectedValue={lista[this.state.pickerSelection].value}
-          mode='dropdown'
-          style={{height: 80, width: 320, alignSelf: 'center'}}
-          onValueChange={(itemValue, itemIndex) =>
-            {this.setState({pickerSelection: itemIndex})}}
-        >
-          {lista.map(element =>{
-            return (
-                <Picker.Item key={element.value} label={element.label} value={element.value}/>
-            );
-          })}
-        </Picker>
+        <Potvrde/>
       </View>
     );
   }
---
+++ MobilnaAplikacija/India/Potvrde.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View, Picker } from 'react-native';
+import RF from "react-native-responsive-fontsize"
+ 
+
+class Potvrde extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pickerSelection: 0
+    }
+  }
+
+  render() {
+    const lista = [{label: "Potvrda o redovnom studiju", value: "potvrda"},   
+                  {label: "Uvjerenje o položenim ispitima", value: "uvjerenje"}];
+    return (
+      <View>
+        <Text style={{fontSize: RF(3.5), margin: 70, alignSelf: 'center'}}>Zahtjev za izdavanje ovjerenog uvjerenja</Text>
+        <Text style={{fontSize: RF(2.5), alignSelf: 'center'}}>Izaberite tip potvrde: </Text>
+        <Picker
+          selectedValue={lista[this.state.pickerSelection].value}
+          mode='dropdown'
+          style={{height: 80, width: 320, alignSelf: 'center'}}
+          onValueChange={(itemValue, itemIndex) =>
+            {this.setState({pickerSelection: itemIndex})}}
+        >
+          {lista.map(element =>{
+            return (
+                <Picker.Item key={element.value} label={element.label} value={element.value}/>
+            );
+          })}
+        </Picker>
+      </View>
+    );
+  }
+}
+export default Potvrde;
+/* const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+*/
