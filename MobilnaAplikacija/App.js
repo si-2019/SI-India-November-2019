99f76f1703cf2f35b9a5f0475d2e80ca347eb185
Edvin Teskeredzic <eteskeredz1@etf.unsa.ba>
2019-04-28 23:30:11 +0200

    Update

--- MobilnaAplikacija/App.js
+++ MobilnaAplikacija/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import AktivniIspiti from './src/components/AktivniIspiti'
+import AktivniIspiti from './India/AktivniIspiti'
 
 
 export default class App extends React.Component {
