1e2c73302377beee9be9e1e438a040404dfe6e8e
aldin-celik <acelik3@etf.unsa.ba>
2019-04-30 20:19:23 +0200

    dodan server november

--- MobilnaAplikacija/November/server.js
+++ MobilnaAplikacija/November/server.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+
 const app = express();
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.listen(31914);
