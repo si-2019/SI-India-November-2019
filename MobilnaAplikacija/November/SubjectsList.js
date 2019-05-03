0213b7b710b61f693a8381048650f4144c2493ef
ekapetanovic1 <ekapetanov1@etf.unsa.ba>
2019-05-03 21:07:58 +0200

    Created SubjectsList component to display the list of subjects

---
+++ MobilnaAplikacija/November/SubjectsList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
+
+export class SubjectsList extends React.Component {
+    
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         
+      }
+    }
+    
+    render() {
+        return (
+            <View>
+                <Text>This is a SubjectsList component!</Text>
+            </View>
+        );
+    }
+}
