46c77046c302a6d302505d5943f5baf45170f830
ekapetanovic1 <ekapetanov1@etf.unsa.ba>
2019-05-03 23:04:39 +0200

    Implemented FlatList within SubjectsList component

--- MobilnaAplikacija/November/Dashboard.js
+++ MobilnaAplikacija/November/Dashboard.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-
+import { SubjectsList } from './SubjectsList';
+import { Divider } from 'react-native-elements';
 
 export default class Dashboard extends Component {
   render() {
     return (
       <View style={styles.MainContainer}>
          {/* <Text style={{ fontSize: 23 }}> Dashboard </Text> */} 
-         
+         <View style={styles.subjectsContainer}>
+           <SubjectsList />
+         </View>
+         <Divider style={styles.divider} />
+         <View style={styles.notificationsContainer}>
+           <Text>Notifications component!</Text>
+         </View>
       </View>
     );
   }
@@ -17,9 +24,24 @@ export default class Dashboard extends Component {
 const styles = StyleSheet.create({
   MainContainer: {
     flex: 1,
-    paddingTop: 20,
     alignItems: 'center',
-    marginTop: 50,
-    justifyContent: 'center',
+    marginTop: 5,
+    marginBottom: 5
   },
+  subjectsContainer: {
+    height: '30%',
+    // backgroundColor: 'skyblue',
+    justifyContent: 'center'
+  },
+  divider: {
+    width: '90%',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black'
+  },
+  notificationsContainer: {
+    height: '60%',
+    marginTop: 30,
+    // backgroundColor: 'powderblue',
+    justifyContent: 'center'
+  }
 });
--- MobilnaAplikacija/November/SubjectsList.js
+++ MobilnaAplikacija/November/SubjectsList.js
@@ -7,15 +7,60 @@ export class SubjectsList extends React.Component {
       super(props)
     
       this.state = {
-         
+         subjects: []
       }
+    }
+
+    componentDidMount() {
+        this.setState({
+            subjects: getSubjects
+        });
     }
     
     render() {
         return (
             <View>
-                <Text>This is a SubjectsList component!</Text>
+                <FlatList
+                    data={this.state.subjects}
+                    keyExtractor={item => item.id.toString()}
+                    renderItem={({item}) => (
+                        <Text style={styles.item}>
+                            {item.title}
+                        </Text>
+                    )}
+                />
             </View>
         );
     }
 }
+
+const getSubjects = [
+    {
+        id: 1,
+        title: "Organizacija softverskog projekta"
+    },
+    {
+        id: 2,
+        title: "Projektovanje informacionih sistema"
+    },
+    {
+        id: 3,
+        title: "Softver inženjering"
+    },
+    {
+        id: 4,
+        title: "Vještačka inteligencija"
+    },
+    {
+        id:5,
+        title: "Završni rad"
+    }
+]
+
+const styles = StyleSheet.create({
+    item: {
+      padding: 5,
+      fontSize: 16,
+      height: 32,
+    }
+});
