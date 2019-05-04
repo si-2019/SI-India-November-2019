81e7d0e40a4f88ec5e0dbbe67cda6657528c43f6
Ajla Panjeta <apanjeta2@etf.unsa.ba>
2019-05-04 17:08:14 +0200

    Omoguceno dodavanje slike na profilu sttudenta

--- MobilnaAplikacija/November/Student.js
+++ MobilnaAplikacija/November/Student.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import PodaciOStudentu from './PodaciOStudentu';
+import Slika from './Slika';
 
 const Student = (props) => {
   return (
-  <ScrollView>
+  <ScrollView>   
+      <Slika />
       <PodaciOStudentu/>
   </ScrollView>
 )
