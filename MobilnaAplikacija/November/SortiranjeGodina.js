4f9bca8f80ec5cf7e2f2bd38bf75f860e0ca1d28
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2019-05-04 20:23:11 +0200

    Kreirana sortirana lista

--- MobilnaAplikacija/November/SortiranjeGodina.js
+++ MobilnaAplikacija/November/SortiranjeGodina.js
@@ -7,8 +7,10 @@ import {
     Text,
     TextInput,
     TouchableOpacity,
-    View, FlatList,
+    View,
+    FlatList,
 } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
 // import axios from 'axios';
 
 export default class SortiranjeGodina extends Component {
@@ -25,18 +27,66 @@ export default class SortiranjeGodina extends Component {
         this.state = {
             prva: [6, 6, 6, 7, 6, 9, 8, 8, 8, 6],
             druga: [6, 7, 8, 8, 7, 9, 7, 8, 8, 7, 7, 9],
-            treca: [10, 9, 9, 8, 7, 7, 9, 8, 7, 6]
+            treca: [10, 9, 9, 8, 7, 7, 9, 8, 7, 6],
+            godine:[]
         }
+    }
+
+    componentDidMount() {
+        /* axios.get("http://localhost:3000/subjects")
+            .then(res => {
+                const newSubjects = res.data;
+                this.setState({ subjects: newSubjects });
+            })
+            .then(error => {
+                console.error(error);
+            }
+        ) */
+        this.setState({
+            godine: getMarks
+        });
     }
 
     render() {
         var prosjekPrve = this.racunanjeProsjeka(this.state.prva);
         var prosjekDruge = this.racunanjeProsjeka(this.state.druga);
         var prosjekTrece = this.racunanjeProsjeka(this.state.treca);
-        var prosjeci    
+       // var Array_2 = ["Prva godina": prosjekPrve, "Druga godina": prosjekDruge, "Treca godina": prosjekTrece];
+
+        // Sorting array in Descending order.
+
+        var godineProsjek = [
+            {
+                godina : 'Prva godina',
+                prosjek : prosjekPrve
+            },
+            {
+                godina : 'Druga godina',
+                prosjek : prosjekDruge
+            },
+            {
+                godina : 'Treća godina',
+                prosjek : prosjekTrece
+            },
+        ];
+        godineProsjek.sort(function(a,b){
+            return parseInt(b.prosjek)  - parseInt(a.prosjek);
+        })
+       // Array_2.sort(function(a, b){return b-a});
+        console.log(godineProsjek[0].godina);
         return (
             <View style={styles.MainContainer}>
-                <Text style={{ fontSize: 18,  fontWeight: 'bold' }}> Prosjeci po godinama sortirani</Text>
+                <Text style={{ fontSize: 18,  fontWeight: 'bold' }}> Projeci po godinama sortirani</Text>
+              /*  <FlatList
+                    data = {[
+                        {key:godineProsjek[0].godina, value:godineProsjek[0].prosjek},
+                        {key:godineProsjek[1].godina, value:godineProsjek[1].prosjek},
+                        {key:godineProsjek[2].godina, value:godineProsjek[2].prosjek}
+                    ]}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key} : {item.value}</Text>}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key} : {item.value}</Text>}
+                    renderItem={({item}) => <Text style={styles.item}>{item.key} : {item.value}</Text>}
+                />*/
 
             </View>
         );
@@ -45,15 +95,15 @@ export default class SortiranjeGodina extends Component {
 const getMarks = [
     {
         id: 1,
-        title: "Prva godina",
+        title: 'alma'
     },
     {
         id: 2,
-        title: "Druga godina"
+        title: 'alma'
     },
     {
-        id: 2,
-        title: "Treca godina"
+        id: 3,
+        title: 'alma'
     }
 ]
 const styles = StyleSheet.create({
@@ -61,4 +111,9 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingTop: 15
     },
+    item: {
+        padding: 5,
+        fontSize: 16,
+        height: 32,
+    }
 });
