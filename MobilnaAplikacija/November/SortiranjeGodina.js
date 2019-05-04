b70643bec6a838a78aade0976a8f3c829b0d7dc8
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2019-05-04 18:13:24 +0200

    Dodan konstruktor za godine

--- MobilnaAplikacija/November/SortiranjeGodina.js
+++ MobilnaAplikacija/November/SortiranjeGodina.js
@@ -9,25 +9,44 @@ import {
     TouchableOpacity,
     View,
 } from 'react-native';
-
+// import axios from 'axios';
 
 export default class SortiranjeGodina extends Component {
-    //Komponenta za izvještaje
+    constructor(props) {
+        super(props)
+        this.state = {
+            prva: [6, 6, 6, 7, 6, 9, 8, 8, 8, 6],
+            druga: [6, 7, 8, 8, 7, 9, 7, 8, 8, 7, 7, 9],
+            treca: [10, 9, 9, 8, 7, 7, 9, 8, 7, 6]
+        }
+    }
+
     render() {
         return (
             <View style={styles.MainContainer}>
-                <Text style={{ fontSize: 23 }}> Sortiranje </Text>
+                <Text style={{ fontSize: 18,  fontWeight: 'bold' }}> Prosjeci po godinama sortirani</Text>
+
             </View>
         );
     }
 }
-
+const getMarks = [
+    {
+        id: 1,
+        title: "Prva godina"
+    },
+    {
+        id: 2,
+        title: "Druga godina"
+    },
+    {
+        id: 2,
+        title: "Treca godina"
+    }
+]
 const styles = StyleSheet.create({
     MainContainer: {
         flex: 1,
-        paddingTop: 20,
-        alignItems: 'center',
-        marginTop: 50,
-        justifyContent: 'center',
+        paddingTop: 15
     },
 });
