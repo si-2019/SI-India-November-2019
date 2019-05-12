6605974f67e83206623dcb4250f54c090d4a31e1
miiden <midhathodo@gmail.com>
2019-05-12 12:21:46 +0200

    Dodana komponenta Ispiti i uvezana sa listom

--- MobilnaAplikacija/November/Predmet/Ispit.js
+++ MobilnaAplikacija/November/Predmet/Ispit.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {View,Text} from 'react-native';
+
 const Ispit = (props) => {
-    const {naziv,datum,bodovi}=props;
+    console.log(props.ispit);
+    const {naziv,datum,bodovi}=props.ispit;
   return (
-    <Text>{naziv}({datum}): {bodovi}</Text>
+    <Text>{naziv}({datum}): {bodovi} bodova</Text>
   )
 }
 
--- MobilnaAplikacija/November/SubjectsList.js
+++ MobilnaAplikacija/November/SubjectsList.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator
+} from "react-native";
 // import axios from 'axios';
 
 export class SubjectsList extends React.Component {
-    
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-         subjects: []
-      }
-    }
+  constructor(props) {
+    super(props);
 
-    componentDidMount() {
-        /* axios.get("http://localhost:3000/subjects")
+    this.state = {
+      subjects: []
+    };
+  }
+
+  componentDidMount() {
+    /* axios.get("http://localhost:3000/subjects")
             .then(res => {
                 const newSubjects = res.data;
                 this.setState({ subjects: newSubjects });
@@ -22,82 +28,109 @@ export class SubjectsList extends React.Component {
                 console.error(error);
             }
         ) */
-        this.setState({
-            subjects: getSubjects
-        });
-    }
-    render() {
-        return (
-            <View>
-                <FlatList
-                    data={this.state.subjects}
-                    keyExtractor={item => item.id.toString()}
-                    renderItem={({item}) => (
-                        <TouchableOpacity >
-                            <Text style={styles.item} onPress={()=>this.props.navigation.navigate("Predmet",item)}>
-                                {item.title}
-                            </Text>
-                        </TouchableOpacity>
-                    )}
-                />
-            </View>
-        );
-    }
+    this.setState({
+      subjects: getSubjects
+    });
+  }
+  render() {
+    return (
+      <View>
+        <FlatList
+          data={this.state.subjects}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity>
+              <Text
+                style={styles.item}
+                onPress={() => this.props.navigation.navigate("Predmet", item)}
+              >
+                {item.title}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+  }
 
-    _getSubjectsFromApi = async (endpoint) => {
-        try {
-          const response = await fetch(endpoint);
-          const data = await response.json();
-          return data;
-        }
-        catch(error) {
-          console.error(error);
-        }
+  _getSubjectsFromApi = async endpoint => {
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
     }
+  };
 }
 
 const getSubjects = [
-    {
-        id: 1,
-        title: "Organizacija softverskog projekta",
-        profesor:"Profesor OSP",
-        ECTS:"4",
-        asistenti:"Asistent"
-    },
-    {
-        id: 2,
-        title: "Projektovanje informacionih sistema",
-        profesor:"Profesor PIS",
-        ECTS:"4",
-        asistenti:"Asistent"
-    },
-    {
-        id: 3,
-        title: "Softver inženjering",
-        profesor:"Profesor SI",
-        ECTS:"4",
-        asistenti:"Asistent"
-    },
-    {
-        id: 4,
-        title: "Vještačka inteligencija",
-        profesor:"Profesor VI",
-        ECTS:"4",
-        asistenti:"Asistent"
-    },
-    {
-        id:5,
-        title: "Završni rad",
-        profesor:"Profesor ZR",
-        ECTS:"4",
-        asistenti:"Asistent"
-    }
-]
+  {
+    id: 1,
+    title: "Organizacija softverskog projekta",
+    profesor: "Profesor OSP",
+    ECTS: "4",
+    asistenti: "Asistent",
+    ispiti: [
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 13 },
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 15 },
+      { naziv: "Drugi parcijalni", datum: "24/11/2019", bodovi: 13 }
+    ]
+  },
+  {
+    id: 2,
+    title: "Projektovanje informacionih sistema",
+    profesor: "Profesor PIS",
+    ECTS: "4",
+    asistenti: "Asistent",
+    ispiti: [
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 13 },
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 15 },
+      { naziv: "Drugi parcijalni", datum: "24/11/2019", bodovi: 13 }
+    ]
+  },
+  {
+    id: 3,
+    title: "Softver inženjering",
+    profesor: "Profesor SI",
+    ECTS: "4",
+    asistenti: "Asistent",
+    ispiti: [
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 13 },
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 15 },
+      { naziv: "Drugi parcijalni", datum: "24/11/2019", bodovi: 13 }
+    ]
+  },
+  {
+    id: 4,
+    title: "Vještačka inteligencija",
+    profesor: "Profesor VI",
+    ECTS: "4",
+    asistenti: "Asistent",
+    ispiti: [
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 13 },
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 15 },
+      { naziv: "Drugi parcijalni", datum: "24/11/2019", bodovi: 13 }
+    ]
+  },
+  {
+    id: 5,
+    title: "Završni rad",
+    profesor: "Profesor ZR",
+    ECTS: "4",
+    asistenti: "Asistent",
+    ispiti: [
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 13 },
+      { naziv: "Prvi parcijalni", datum: "11/11/2019", bodovi: 15 },
+      { naziv: "Drugi parcijalni", datum: "24/11/2019", bodovi: 13 }
+    ]
+  }
+];
 
 const styles = StyleSheet.create({
-    item: {
-      padding: 5,
-      fontSize: 16,
-      height: 32,
-    }
+  item: {
+    padding: 5,
+    fontSize: 16,
+    height: 32
+  }
 });
