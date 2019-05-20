b876f2c16fcefd8a9f576bc6fa83689f0b353ea0
ndardagan <nadja_dar@hotmail.com>
2019-05-21 00:33:32 +0200

    Dodan status potvrde i prilagodjeno brisanje na osnovu statusa

--- MobilnaAplikacija/India/prikazPotvrda.js
+++ MobilnaAplikacija/India/prikazPotvrda.js
@@ -7,23 +7,30 @@ const extractKey = ({key}) => key
 class Zahtjev extends React.Component {
     state = {
         data: [
-            {key: 'Potvrda o regulisanju stipendije', value: '25.01.2019'}, {key: 'Potvrda o regulisanju zdravstvenog osiguranja', value:'02.02.2019.'}
+            {key: 'Potvrda o regulisanju stipendije', value: '25.01.2019', status: 'Neobrađen'}, {key: 'Potvrda o regulisanju zdravstvenog osiguranja', value:'02.02.2019.', status: 'Obrađen'}
         ],
     }
-    ponistiZahtjev(zahtjev) {
+    ponistiZahtjev(zahtjev, status) {
+        console.log("status: " + status);
         Alert.alert(
             'Da li ste sigurni da želite poništiti zahtjev za potvrdu',
+            
             zahtjev,
             [
-                {text: 'Poništi', onPress: () => this.deleteItemByKey(zahtjev)},
+                {text: 'Poništi', onPress: () => this.deleteItemByKey(zahtjev, status)},
                 {text: 'Odustani', onPress: () => console.log('Odustao'), style: 'cancel'},
             ],
               {cancelable: false}
         );
     }
-    deleteItemByKey = key => {
-        const filteredData = this.state.data.filter(item => item.key !== key);
-        this.setState({ data: filteredData });
+    deleteItemByKey(key, status) {
+        console.log(status);
+        if(status != 'Obrađen') {
+            console.log("TU");
+            const filteredData = this.state.data.filter(item => item.key !== key);
+            this.setState({ data: filteredData });
+        }
+        else Alert.alert("Ne možete poništiti obrađenu potvrdu");
         console.log(this.state.data);
     }
     
@@ -35,8 +42,10 @@ class Zahtjev extends React.Component {
                 <Text style ={styles.itemT}> {item.key}</Text> 
                 <Text style ={styles.itemText}>Datum izdavanja: </Text>
                 <Text style={styles.itemT}> {item.value}</Text>
+                <Text style ={styles.itemText}>Status: </Text>
+                <Text style={styles.itemT}> {item.status}</Text>
                 <View style={styles.dugmic}>
-                    <Button onPress={() => this.ponistiZahtjev(item.key)}  title="Poništi zahtjev"/>
+                    <Button onPress={() => this.ponistiZahtjev(item.key, item.status)}  title="Poništi zahtjev"/>
                 </View>
             </View>
         );
