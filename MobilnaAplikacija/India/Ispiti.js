ef527618c9ce4f9612d1463686cd789f85c0bec2
ndardagan <nadja_dar@hotmail.com>
2019-05-22 22:26:06 +0200

    Popravljeno mijenjanje teksta prijava/odjava i dodan filter popunjenih ispita

--- MobilnaAplikacija/India/Ispiti.js
+++ MobilnaAplikacija/India/Ispiti.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 //import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import {
@@ -8,66 +7,77 @@ import {
 } from 'react-navigation';
 import {Alert,Text, View, Image, TouchableOpacity, StyleSheet, Button } from 'react-native';
 import moment from 'moment';
-let inicijalni ={ispiti_info:[]} 
+
+let inicijalni ={ispiti_info:[], ispiti: [{ key: 0, predmet: "Vjestacka inteligencija", tip: "Prvi parcijalni", datum: "10.2.2019. 13:00", aktivan: 1, prijavljen: 1, popunjen: 0 },
+{ key: 1, predmet: "Organizacija softverskog projekta", tip: "Drugi parcijalni", datum: "13.6.2019. 18:00", aktivan: 1, prijavljen: 0, popunjen: 0 },
+{ key: 2, predmet: "Softverski inzenjering", tip: "Prvi parcijalni", datum: "15.6.2019. 10:30", aktivan: 0, prijavljen: 1, popunjen: 1 },
+{ key: 3, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 13:00", aktivan: 1, prijavljen: 0, popunjen: 0 },
+{ key: 4, predmet: "Projektovanje informativnih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan: 1, prijavljen: 1, popunjen: 1 }], 
+kopijaIspiti: []
+} 
 let notifikacije = true;
-  class AktivniIspiti extends React.Component {
+
+class AktivniIspiti extends React.Component {
+    
     constructor(props) {
         super(props);
         this.state = inicijalni
         inicijalizovanjeIspita = () => {
             this.setState(inicijalni)
         }
+        this.promijeniTekstButtona = this.promijeniTekstButtona.bind(this);
+        this.filtrirajPopunjene = this.filtrirajPopunjene.bind(this);
     }
-    detalji()
-	{
-    Alert.alert(
-      'Detalji o ispitu',
-      'Profesor: Richard Feynman \nSala: MA',
-      [
-        {text: 'Nazad'},
-      ],
-        {cancelable: true}
-    );
-  }
+    
+    detalji() {
+      Alert.alert(
+        'Detalji o ispitu',
+        'Profesor: Richard Feynman \nSala: MA',
+        [
+          {text: 'Nazad'},
+        ],
+          {cancelable: false}
+      );
+    }
+
     promijeniTekstButtona = i => {
-        this.setState(state => {
-            const list = state.ispiti_info.map((item, j) => {
+        this.setState(() => {
+            const list = this.state.ispiti_info.map((item, j) => {
+              
                 if (j === i) {
-                    if (item.prijavaTekst === 'Prijavi')
+                    if (item.prijavaTekst === 'Prijavi') 
                         item.prijavaTekst = 'Odjavi'
                     else
-                        item.prijavaTekst = 'Prijavi'
-                    return item;
-                } else {
-                    return item;
-                }
+                        item.prijavaTekst = 'Prijavi'   
+                } 
+                return item;
             });
-
             return {
                 list,
             };
         });
     };
 
+    filtrirajPopunjene() {
+      this.setState({kopijaIspiti: this.state.ispiti}, () => {
+        this.setState({ispiti: this.state.ispiti.filter(ispit=>  ispit.popunjen !== 1)});
+      });
+    }
+
     render() {
-
-        let ispiti = [{ id: 0, predmet: "Vjestacka inteligencija", tip: "Prvi parcijalni", datum: "10.2.2019. 13:00", aktivan: 1, prijavljen: 1, popunjen: 0 },
-        { id: 1, predmet: "Organizacija softverskog projekta", tip: "Drugi parcijalni", datum: "13.6.2019. 18:00", aktivan: 1, prijavljen: 0, popunjen: 0 },
-        { id: 2, predmet: "Softverski inzenjering", tip: "Prvi parcijalni", datum: "15.6.2019. 10:30", aktivan: 0, prijavljen: 1, popunjen: 0 },
-        { id: 3, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan: 1, prijavljen: 0, popunjen: 0 },
-        { id: 4, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan: 1, prijavljen: 1, popunjen: 0 }];
-
+      
+        
         let k = 0;
-        ispiti.map((ispit) => {
+        this.state.ispiti.map((ispit) => {
             let porukaa;
             if (ispit.prijavljen) porukaa = 'Odjavi'
             else porukaa = 'Prijavi'
-            inicijalni.ispiti_info[k] = { ind: k, id: ispit.id, prijavaTekst: porukaa }
+            inicijalni.ispiti_info[k] = { ind: k, key: ispit.key, prijavaTekst: porukaa }
             k++;
         })
         this.inicijalizovanjeIspita;
         let j, greska;
-        ispisAktivnihIspita = ispiti.map((ispit) => {
+        ispisAktivnihIspita = this.state.ispiti.map((ispit) => {
             const dateLimit = moment(ispit.datum, 'DD.MM.YYYY hh:mm');
             const now = moment();
 
@@ -79,12 +89,13 @@ let notifikacije = true;
                         <View style={styles.elementi_tabele}><Text style={styles.elementi_tabele_tekst}>{ispit.tip}</Text></View>
                         <View style={styles.elementi_tabele}><Text style={styles.elementi_tabele_tekst}>{ispit.datum}</Text></View>
                         <View style={styles.elementi_tabele}>
-                            <TouchableOpacity  >
+                            <TouchableOpacity>
                                 <Text style={[styles.elementi_tabele_tekst, { color: 'blue' }]} onPress={() => {
-                                    j = ispiti.indexOf(ispit);
+                                    j = this.state.ispiti.indexOf(ispit);
+                                    
                                     greska = 0;
-                                    ispiti.map((item) => {
-                                        if (item.id != ispit.id && item.predmet === ispit.predmet && item.tip === item.tip && item.prijavljen == 1) {
+                                    this.state.ispiti.map((item) => {
+                                        if (item.key != ispit.key && item.predmet === ispit.predmet && item.tip === item.tip && item.prijavljen == 1) {
                                             greska = 1;
                                         }
                                     });
@@ -92,29 +103,24 @@ let notifikacije = true;
                                         alert('Prijavljeni ste na drugi termin ovog ispita, odjavite ga kako bi se mogli prijavili na ovaj!');
                                     }
                                     else if (ispit.prijavljen) {
-                                        ispiti[j].prijavljen = 0;
-                                        console.log(j)
+                                        this.state.ispiti[j].prijavljen = 0;
+                                        
                                         this.promijeniTekstButtona(j);
-                                        alert('Ispit odjavljen!');
+                                        alert('Ispit uspješno odjavljen!');
                                     }
                                     else {
-                                        ispiti[j].prijavljen = 1;
-                                        console.log(j)
+                                        this.state.ispiti[j].prijavljen = 1;
                                         this.promijeniTekstButtona(j);
-                                        alert('Ispit prijavljen!');
+                                        alert('Ispit uspješno prijavljen!');
                                     }
 
-                                }} >{this.state.ispiti_info[ispiti.indexOf(ispit)].prijavaTekst}</Text>
+                                }}>{this.state.ispiti_info[this.state.ispiti.indexOf(ispit)].prijavaTekst}</Text>
                             </TouchableOpacity></View>
                         {/* <View style={styles.elementi_tabele}><Text style={styles.elementi_tabele_tekst} onPress={this.handleClick} color = 'blue'>{prijava}</Text></View>
             <Button onPress={this.handleClick} title={prijava} type="clear" />*/}
                     </View>
-
                 );
-
-
             }
-
         })
 
         return (
@@ -127,13 +133,15 @@ let notifikacije = true;
                     <View style={styles.header}><Text style={styles.header_tekst}>Prijava</Text></View>
                 </View>
                 <View>
-                    {ispisAktivnihIspita}
+                    {ispisAktivnihIspita
+                    }
                 </View>
+                <Button onPress={() => this.filtrirajPopunjene()} title="Sakrij popunjene termine"/>
             </View>
         );
     }
 }
-  class PrijavljeniIspiti extends React.Component {
+class PrijavljeniIspiti extends React.Component {
     constructor(props){
       super(props);
       this.state = inicijalni
@@ -143,6 +151,7 @@ let notifikacije = true;
       }
       l=0;
     }
+
     OdjaviIspitIzPrijavljenih= i => {
       this.setState(state => {
         const list = state.ispiti_info.filter((item, j) => i !== j);
@@ -152,19 +161,20 @@ let notifikacije = true;
         };
       });
     };
+    
     render() {
-      let ispiti = [{id: 0, predmet: "Vjestacka inteligencija", tip: "Prvi parcijalni", datum: "10.2.2019. 13:00", aktivan:1, prijavljen: 1, popunjen:0},
-      {id:1, predmet: "Organizacija softverskog projekta", tip: "Drugi parcijalni", datum: "13.6.2019. 18:00", aktivan:1, prijavljen: 0, popunjen:0},
-      {id:2, predmet: "Softverski inzenjering", tip: "Prvi parcijalni", datum: "15.6.2019. 10:30", aktivan:0, prijavljen: 1, popunjen:0},
-      {id:3, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan:1, prijavljen: 0, popunjen:0},
-      {id:4, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan:1, prijavljen: 1, popunjen:0}];
+      let ispiti = [{key: 0, predmet: "Vjestacka inteligencija", tip: "Prvi parcijalni", datum: "10.2.2019. 13:00", aktivan:1, prijavljen: 1, popunjen:0},
+      {key:1, predmet: "Organizacija softverskog projekta", tip: "Drugi parcijalni", datum: "13.6.2019. 18:00", aktivan:1, prijavljen: 0, popunjen:0},
+      {key:2, predmet: "Softverski inzenjering", tip: "Prvi parcijalni", datum: "15.6.2019. 10:30", aktivan:0, prijavljen: 1, popunjen:0},
+      {key:3, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan:1, prijavljen: 0, popunjen:0},
+      {key:4, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan:1, prijavljen: 1, popunjen:0}];
        
        let k=0;
        ispiti.map((ispit)=>{
          let porukaa;
          if(ispit.prijavljen) porukaa='Odjavi'
          else porukaa='Prijavi'
-         inicijalni.ispiti_info[k]={ind:k, id:ispit.id,  prijavaTekst: porukaa}
+         inicijalni.ispiti_info[k]={ind:k, key:ispit.key,  prijavaTekst: porukaa}
          k++;
        })
        this.inicijalizovanjeIspita;
@@ -186,7 +196,7 @@ let notifikacije = true;
             <Text style={[styles.elementi_tabele_tekst, {color:'blue'}]} onPress={()=>{
               j=ispiti.indexOf(ispit);
               ispiti[j].prijavljen=0;
-              console.log(j)
+              
               this.OdjaviIspitIzPrijavljenih(j)               
               alert('Ispit odjavljen!');
              
@@ -206,19 +216,21 @@ let notifikacije = true;
         return (
           
             <View style={styles.container}>
-            <View style={styles.header_container}>
-              <View style={styles.header}><Text style={styles.header_tekst}>Predmet</Text></View>
-              <View style={styles.header}><Text style={styles.header_tekst}>Tip</Text></View>
-              <View style={styles.header}><Text style={styles.header_tekst}>Datum</Text></View>
-              <View style={styles.header}><Text style={styles.header_tekst}>Prijava</Text></View>
-            </View>
-            <View>
-                {ispisAktivnihIspita}
-            </View>
+              <View style={styles.header_container}>
+                <View style={styles.header}><Text style={styles.header_tekst}>Predmet</Text></View>
+                <View style={styles.header}><Text style={styles.header_tekst}>Tip</Text></View>
+                <View style={styles.header}><Text style={styles.header_tekst}>Datum</Text></View>
+                <View style={styles.header}><Text style={styles.header_tekst}>Prijava</Text></View>
+              </View>
+              <View>
+                  {//ispisAktivnihIspita
+                  }
+              </View>
             </View>
         );
     }
   }
+
 class SviPrijavljeniIspiti extends React.Component {
   constructor(props){
     super(props);
@@ -259,18 +271,18 @@ class SviPrijavljeniIspiti extends React.Component {
   };
 
   render() {     
-      let ispiti = [{id: 0, predmet: "Vjestacka inteligencija", tip: "Prvi parcijalni", datum: "10.2.2019. 13:00", aktivan:1, prijavljen: 1, popunjen:0},
-     {id:1, predmet: "Organizacija softverskog projekta", tip: "Drugi parcijalni", datum: "13.6.2019. 18:00", aktivan:1, prijavljen: 0, popunjen:0},
-     {id:2, predmet: "Softverski inzenjering", tip: "Prvi parcijalni", datum: "15.6.2019. 10:30", aktivan:0, prijavljen: 1, popunjen:0},
-     {id:3, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan:1, prijavljen: 0, popunjen:0},
-     {id:4, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan:1, prijavljen: 1, popunjen:0}];
+      let ispiti = [{key: 0, predmet: "Vjestacka inteligencija", tip: "Prvi parcijalni", datum: "10.2.2019. 13:00", aktivan:1, prijavljen: 1, popunjen:0},
+     {key:1, predmet: "Organizacija softverskog projekta", tip: "Drugi parcijalni", datum: "13.6.2019. 18:00", aktivan:1, prijavljen: 0, popunjen:0},
+     {key:2, predmet: "Softverski inzenjering", tip: "Prvi parcijalni", datum: "15.6.2019. 10:30", aktivan:0, prijavljen: 1, popunjen:0},
+     {key:3, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan:1, prijavljen: 0, popunjen:0},
+     {key:4, predmet: "Projektovanje informacionih sistema", tip: "Usmeni", datum: "16.6.2019. 11:00", aktivan:1, prijavljen: 1, popunjen:0}];
      
      let k=0;
      ispiti.map((ispit)=>{
        let porukaa;
        if(ispit.prijavljen) porukaa='Odjavi'
        else porukaa='Prijavi'
-       inicijalni.ispiti_info[k]={ind:k, id:ispit.id,  prijavaTekst: porukaa}
+       inicijalni.ispiti_info[k]={ind:k, key:ispit.key,  prijavaTekst: porukaa}
        k++;
      })
      this.inicijalizovanjeIspita;
@@ -307,7 +319,8 @@ class SviPrijavljeniIspiti extends React.Component {
             <View style={styles.header}><Text style={styles.header_tekst}>Datum</Text></View>
           </View>
           <View>
-              {ispisAktivnihIspita}
+              {//ispisAktivnihIspita
+              }
           </View>
           </View>
       );
@@ -336,7 +349,7 @@ const TabScreen = createMaterialTopTabNavigator(
       },
       indicatorStyle: {
         borderBottomColor: '#376ff2',
-        borderBottomWidth: 2,
+        borderBottomWkeyth: 2,
       },
     },
   }
