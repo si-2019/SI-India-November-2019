717dc8a450710b67c7e7a7d9d167a433b534d3fe
aldin-celik <acelik3@etf.unsa.ba>
2019-05-23 03:24:47 +0200

    ucitani predmeti

--- MobilnaAplikacija/November/odslusaniPredmeti.js
+++ MobilnaAplikacija/November/odslusaniPredmeti.js
@@ -3,15 +3,48 @@ import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 
-export default class Izvjestaji extends Component {
+export default class odslusaniPredmeti extends Component {
+  
+  cosntructor(props)
+  {
+      
+      this.state =
+      {
+           lista: []
+      }
+  }
+
+  dohvatiPredmete()
+    {
+         this.state.lista = 
+         [
+             {
+                Naziv:"Zimski semestar 2016/2017",
+                Predmeti:
+                [
+                    {predmet: "Inženjerska matematika 1"},
+                    {predmet: "Inženjerska fizika 1"},
+                    {predmet: "Linearna algebra i geometrija"},
+                    {predmet: "Osnove elektrotehnike"},
+                    {predmet: "Osnove računarstva"}
+                ]
+             },
+             {
+              Naziv:"Ljetni semestar 2016/2017",
+              Predmeti:
+              [
+                  {predmet: "Inženjerska matematika 2"},
+                  {predmet: "Tehnike programiranja"},
+                  {predmet: "Vjerovatnoća i statistika"},
+                  {predmet: "Matematička logika i teorija izračunljivosti"},
+                  {predmet: "Operativni sistemi"}
+              ]
+           }
+         ]
+    }
+  
   render() {
-
-    const studentID = this.props.navigation.state.params;
-
-    
-
-
-    return (
+        return (
       <View style={styles.MainContainer}>
           
       </View>
