2148fced38f90522a69cfd75a8573a1be7f7c878
Amina Kurtovic <akurtovic3@etf.unsa.ba>
2019-05-23 02:17:50 +0200

    potrebno je dobiti upit da li se zeli odjaviti sa termina

--- MobilnaAplikacija/App.js
+++ MobilnaAplikacija/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AktivniIspiti from './India/AktivniIspiti';
+import PrijavljeniIspiti from './India/PrijavljeniIspiti';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <PrijavljeniIspiti/>
     );
   }
 }
