b997f89bc54bea872db36d147cc90c12de5f4362
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2019-06-02 14:24:22 +0200

    Uređivanje stila komponente

--- MobilnaAplikacija/November/Zavrsni.js
+++ MobilnaAplikacija/November/Zavrsni.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import  ReadMore from 'react-native-read-more-text';
 
 class Zavrsni extends React.Component {
+
     render() {
         return (
             <View>
-                <Text>Alma</Text>
+                <Text style={{ fontSize: 18,  fontWeight: 'bold'}}>Tema: Naziv teme</Text>
+                <Text style={{ fontSize: 18,  fontWeight: 'bold'}}>Mentor: prof. dr. John Doe </Text>
+                <Text style={{ fontSize: 18,  fontWeight: 'bold'}}>Sažetak teme: </Text>
+                <ReadMore
+                    numberOfLines={3}
+                    renderTruncatedFooter={this.prikaziVise}
+                    renderRevealedFooter={this.prikaziManje}
+                >
+                <Text style={{ fontSize: 15}}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sociis natoque penatibus et magnis dis parturient montes nascetur. Elementum integer enim neque volutpat ac tincidunt vitae semper. Praesent semper feugiat nibh sed pulvinar proin. Nec feugiat nisl pretium fusce id velit ut. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat. Eget est lorem ipsum dolor sit amet consectetur adipiscing. Quis varius quam quisque id diam vel quam elementum. Sodales neque sodales ut etiam sit amet.
+                 </Text>
+                </ReadMore>
             </View>
         )
     }
+    prikaziVise = (handlePress) => {
+        return (
+            <Text style={{  color: 'blue', marginTop: 5}} onPress={handlePress}>
+                Prikaži više
+            </Text>
+        );
+    }
+    prikaziManje = (handlePress) => {
+        return (
+            <Text style={{color: 'blue', marginTop: 5}} onPress={handlePress}>
+                Prikaži manje
+            </Text>
+        );
+    }
 }
+
 
 export default Zavrsni;
