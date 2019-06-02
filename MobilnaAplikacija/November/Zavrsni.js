f2233195227270507ad2db7d341b25e1864b1fd5
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2019-06-02 13:30:25 +0200

    Dodana opcija odabira završnog rada na početnoj stranici

---
+++ MobilnaAplikacija/November/Zavrsni.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+class Zavrsni extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Alma</Text>
+            </View>
+        )
+    }
+}
+
+export default Zavrsni;
