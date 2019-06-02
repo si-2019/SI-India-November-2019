7c001661d356f8c9378c2f3ca71d8922cf7cad84
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2019-06-02 14:48:44 +0200

    Omogućen upload dokumenta

--- MobilnaAplikacija/November/Zavrsni.js
+++ MobilnaAplikacija/November/Zavrsni.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity ,Button} from 'react-native';
 import  ReadMore from 'react-native-read-more-text';
-
+import { DocumentPicker} from 'expo';
 class Zavrsni extends React.Component {
 
     render() {
         return (
             <View>
                 <Text style={{ fontSize: 18,  fontWeight: 'bold'}}>Tema: Naziv teme</Text>
-                <Text style={{ fontSize: 18,  fontWeight: 'bold'}}>Mentor: prof. dr. John Doe </Text>
-                <Text style={{ fontSize: 18,  fontWeight: 'bold'}}>Sažetak teme: </Text>
+                <Text style={{fontSize: 18,  fontWeight: 'bold'}}>Mentor: prof. dr. John Doe </Text>
+                <Text>{"\n"}</Text>
+                <Text style={{fontSize: 18,  fontWeight: 'bold'}}>Sažetak teme: </Text>
                 <ReadMore
                     numberOfLines={3}
                     renderTruncatedFooter={this.prikaziVise}
@@ -19,6 +20,11 @@ class Zavrsni extends React.Component {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sociis natoque penatibus et magnis dis parturient montes nascetur. Elementum integer enim neque volutpat ac tincidunt vitae semper. Praesent semper feugiat nibh sed pulvinar proin. Nec feugiat nisl pretium fusce id velit ut. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat. Eget est lorem ipsum dolor sit amet consectetur adipiscing. Quis varius quam quisque id diam vel quam elementum. Sodales neque sodales ut etiam sit amet.
                  </Text>
                 </ReadMore>
+
+                <Button
+                    title="Odaberi dokument"
+                    onPress={this.odaberiDokument}
+                />
             </View>
         )
     }
@@ -36,7 +42,9 @@ class Zavrsni extends React.Component {
             </Text>
         );
     }
+    odaberiDokument = async () => {
+        let result = await DocumentPicker.getDocumentAsync({});
+    }
 }
-
 
 export default Zavrsni;
