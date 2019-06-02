dd133047e00d3c55cb113b60852e96d55af8d6bd
aibrasimov1 <[aibrasimov1@etf.unsa.ba]>
2019-06-02 15:15:28 +0200

    Omogućen scroll prilikom dugog teksta

--- MobilnaAplikacija/November/Zavrsni.js
+++ MobilnaAplikacija/November/Zavrsni.js
@@ -6,6 +6,7 @@ class Zavrsni extends React.Component {
 
     render() {
         return (
+            <ScrollView>
             <View>
                 <Text style={{ fontSize: 18,  fontWeight: 'bold'}}>Tema: Naziv teme</Text>
                 <Text style={{fontSize: 18,  fontWeight: 'bold'}}>Mentor: prof. dr. John Doe </Text>
@@ -22,15 +23,28 @@ class Zavrsni extends React.Component {
                 </ReadMore>
                 <Text>{"\n"}</Text>
                 <Text style={{fontSize: 18,  fontWeight: 'bold'}}>Poslani fajlovi</Text>
-                <Text style={{fontSize:15}}>Nije poslan niti jedan dokument. Odaberite novi fajl. </Text>
+                <Text style={{fontSize:15}}>Nije poslan niti jedan dokument.</Text>
+                <Text style={{fontSize:15}}>Odaberite novi dokument. </Text>
                 <TouchableOpacity
                     style={styles.dugme}
                     onPress={
                         () => this.odaberiDokument()
                     }>
-                    <Text style={styles.submitButtonText}> Učitaj dokument </Text>
+                    <Text style={styles.tekst}> Učitaj dokument </Text>
                 </TouchableOpacity>
+                <Text>{"\n"}</Text>
+                <Text style={{fontSize: 18,  fontWeight: 'bold'}}>Komentari i diskusija </Text>
+                <ReadMore
+                    numberOfLines={3}
+                    renderTruncatedFooter={this.prikaziVise}
+                    renderRevealedFooter={this.prikaziManje}
+                >
+                    <Text style={{ fontSize: 15}}>
+                        Lorem ipsum dui sapien eget mi proin sed libero enim sed faucibus Laoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibus Laoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibusLaoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibus. Eros donec ac odio tempor orci dapibus. Senectus et netus et malesuada fames. Auctor elit sed vulputate mi sit amet.
+                    </Text>
+                </ReadMore>
             </View>
+            </ScrollView>
         )
     }
     prikaziVise = (handlePress) => {
@@ -57,8 +71,15 @@ export default Zavrsni;
 const styles = StyleSheet.create({
     dugme: {
         backgroundColor: '#376ff2',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
         padding: 10,
-        margin: 15,
-        height: 40,
+        marginTop: 10,
+        marginLeft: '8%',
+        marginRight: '8%'
+    },
+    tekst:{
+        color:'white'
     }
 });
