63c3d64a49fd6e31c740ff394d57794e77f314bb
Ajla Panjeta <apanjeta2@etf.unsa.ba>
2019-06-03 02:15:17 +0200

    Promjena načina prikaza izvjestaja

--- MobilnaAplikacija/November/SviIzvjestaji/UkupanBrojBodova.js
+++ MobilnaAplikacija/November/SviIzvjestaji/UkupanBrojBodova.js
@@ -1,47 +1,6 @@
 import React, { Component } from 'react';
-import {View,Text,StyleSheet, FlatList, TouchableOpacity} from 'react-native';
+import {View,Text,StyleSheet, FlatList, TouchableOpacity, ScrollView} from 'react-native';
 import axios from 'axios';
-
-/*const UkupanBrojBodova = () => {
-  state= {
-    subjects: []
-  };
-
-  
-pokupiPodatke = () => {
-  axios.get("http://localhost:31914/predmeti/1/ukupnoBodova")
-        .then(res => {
-            this.setState({ subjects: JSON.stringify(res.data[0])});
-            console.log("pozivvv")
-        })
-        .then(error => {
-            console.error(error);
-        }
-    ) 
-}
-  
-  return (
-      <View style={styles.container}>
-        {this.pokupiPodatke()}
-        <Text>Konačna ocjena: {(this.state.subjects)}</Text>
-      </View>
-    )
-  }
-
-const styles = StyleSheet.create({
-    container: {
-        marginTop:5,
-        width: "100%",
-        textAlign:"left"
-    },
-    text:{
-        width:"100%",
-        fontSize:16
-    }
-    ,
-  });
-export default UkupanBrojBodova;*/
-
 
 class UkupanBrojBodova extends Component {
 
@@ -82,13 +41,13 @@ class UkupanBrojBodova extends Component {
 
   render() {
     return (
-      <View >
+      <ScrollView >
       <FlatList
           data={this.state.subjects}
           keyExtractor={item => item.predmet.toString()}
           renderItem={({ item }) => (
+            <View style={styles.Viewitem}>
             <View style={styles.Viewitem1}>
-            <View style={styles.Viewitem2}>
               <Text style={styles.item}>
                 {item.predmet}
               </Text>
@@ -101,7 +60,7 @@ class UkupanBrojBodova extends Component {
               </View>
           )}
         />
-      </View>
+      </ScrollView>
     );
   }
   
@@ -128,7 +87,7 @@ const styles = StyleSheet.create({
     padding: 5,
     margin: 5,
     fontSize: 16,
-    height: 60,
+    height: 80,
   },
   Viewitem: {
     flexDirection: 'row',
@@ -140,8 +99,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#ededed",
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'flex-end',
-    height: '70%'
+    justifyContent: 'flex-start',
+    width: '70%'
   },
   Viewitem2: {
     borderWidth: 1,  
@@ -149,7 +108,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#ededed",
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'flex-start',
+    justifyContent: 'flex-end',
   }
 });
 
