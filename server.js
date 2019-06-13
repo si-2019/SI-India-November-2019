946934f6f240f6ec2e87e54f10e02fcce3713ee5
miiden <midhathodo@gmail.com>
2019-06-13 22:45:03 +0200

    mock data fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 const express = require('express');
 const app = express();
 const https = require('https');
 const request = require('request');
 const axios = require('axios');
 const swagger_document=require('./swagger-document.js');
-import {MOCK_DATA_NOVOSTI, MOCK_DATA_PRVI_PARCIJALNI } from './MOCK_DATA';
+
 var PORT = process.env.PORT || 31914;
 
 //Vraca sve o predmetima koje student trenutno slusa
@@ -183,3 +184,28 @@ swagger_document(app);
 
 app.listen(PORT,function(){ console.log('server successfully started on port '+PORT); });
 
+const MOCK_DATA_NOVOSTI= [
+  { naziv: "Obavijesti1", tekst: "Obavijest1 tekst" },
+  { naziv: "Obavijest2", tekst: "Lorem ipsum dui s apien eget mi proin sed libero enim sed faucibus Laoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibus Laoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibusLaoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibus. Eros donec ac odio tempor orci dapibus. Senectus et netus et malesuada fames. Auctor elit sed vulputate mi sit amet."},
+  { naziv: "Obavijest3", tekst: "Obavijest3 tekst" },
+  { naziv: "Obavijest4", tekst: "Obavijest3 tekst" },
+  { naziv: "Obavijest5", tekst: "Obavijest3 tekst" },
+  { naziv: "Obavijest6", tekst: "Obavijest3 tekst" },
+  { naziv: "Obavijest7", tekst: "Obavijest3 tekst" },
+  { naziv: "Obavijest8", tekst: "Obavijest3 tekst" },
+  { naziv: "Obavijest9", tekst: "Lorem ipsum dui s apien eget mi proin sed libero enim sed faucibus Laoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibus Laoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibusLaoreet id donec ultrices tincidunt. Sagittis id consectetur purus ut faucibus pulvinar. Dui sapien eget mi proin sed libero enim sed faucibus. Eros donec ac odio tempor orci dapibus. Senectus et netus et malesuada fames. Auctor elit sed vulputate mi sit amet."}];
+
+ const MOCK_DATA_PRVI_PARCIJALNI= [
+  {
+    predmet: "Administracija racunarskih mreza",
+    bodovi : 6
+  },
+  {
+    predmet: "Vještačka inteligencija",
+    bodovi : 13
+  },
+  {
+    predmet: "Softver inženjering",
+    bodovi : 17
+  }
+];
